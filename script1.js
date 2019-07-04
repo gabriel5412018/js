5824b09639449ade7aeb9228544aa50485acc2ba
Sydrack Gregório <drasyck1@gmail.com>
2019-07-04 16:59:30 -0300

    adicionado

--- script1.js
+++ script1.js
@@ -1,27 +1,29 @@
-var num = document.getElementById('fnum')
-var lista = document.querySelector('select#flista')
-var res = document.querySelector('div#res')
-var valores = []
-
-function isNumero(n) {
+var num = document.getElementById('fnum') 
+var lista = document.querySelector('select#flista') 
+var res = document.querySelector('div#res') 
+var valores = [] 
+function isNumero(n) { 
     if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
-    } else {
-        return false
-    }
-}
-function  inLista(n,b){
-    var b = 1
-    if (lista.indexOf(Number(n)) !=-1 ) {
-        return true
-  } else {
-      return false
-  }
-}
+         return true 
+        } else { 
+            return false 
+        }
+ } 
+function inLista(numero, valores) { 
+    valores.forEach(valor => { 
+        if (valor == numero) { 
+            console.log(true)
+             return true 
+        } else {
+             console.log(valor) 
+             return false 
+            } }) }
 function add() {
-    if (isNumero(num.value) && !inLista(num.value, valores)){
-       window.alert('ok') 
-    } else{
-        window.alert('Valor invalido ou ja encontrado na lista')
-    }
+     if (isNumero(num.value) && !inLista(num.value, valores)) {
+          window.alert('ok')
+           valores.push(num.value)
+            console.log(valores) 
+        } else { 
+            window.alert('Valor invalido ou ja encontrado na lista') 
+        } 
 }
